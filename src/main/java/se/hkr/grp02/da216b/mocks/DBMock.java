244b4f656422b157e7351758f1d185bb05884741
Christopher Schröter <christopher.schroeter95@gmail.com>
2020-05-06 10:34:26 +0200

    Build a Mock Up Database
    Methods added

--- src/main/java/se/hkr/grp02/da216b/mocks/DBMock.java
+++ src/main/java/se/hkr/grp02/da216b/mocks/DBMock.java
@@ -3,10 +3,40 @@ package se.hkr.grp02.da216b.mocks;
 import se.hkr.grp02.da216b.utilities.Result;
 
 public class DBMock implements IDBMock {
+    private boolean connection;
 
+    @Override
     public Result[] returnResults() {
         return new Result[0];
     }
+
+    @Override
     public void pushResult(Result result) {
+        System.out.println("Results will be pushed to the Database");
+    }
+
+    @Override
+    public boolean connect(String address, String userName, String password) {
+        System.out.println("Connecting to Database...");
+        connection = true;
+        return true;
+    }
+
+    @Override
+    public void disconnect() {
+        System.out.println("Disconnecting from Database...");
+        connection = false;
+    }
+
+    @Override
+    public boolean isConnected() {
+        System.out.print("Is connection alive: ");
+        return connection;
+    }
+
+    @Override
+    public String getName(int id) {
+        System.out.print("Database name: ");
+        return "Database1";
     }
 }
--- src/main/java/se/hkr/grp02/da216b/mocks/IDBMock.java
+++ src/main/java/se/hkr/grp02/da216b/mocks/IDBMock.java
@@ -5,4 +5,9 @@ import se.hkr.grp02.da216b.utilities.Result;
 public interface IDBMock {
     Result[] returnResults();
     void pushResult(Result result);
+    public boolean connect(String address, String userName,String password);
+    public void disconnect();
+    public boolean isConnected();
+    public String getName(int id);
+
 }
