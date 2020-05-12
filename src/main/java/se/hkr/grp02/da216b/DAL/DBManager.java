dd123b425481ba0988951a6833a7058f796522fa
Vincenzo <vincebshock@gmail.com>
2020-05-12 10:19:32 +0200

    DBManager: in-class connection test added

--- src/main/java/se/hkr/grp02/da216b/DAL/DBManager.java
+++ src/main/java/se/hkr/grp02/da216b/DAL/DBManager.java
@@ -1,8 +1,14 @@
 package se.hkr.grp02.da216b.DAL;
 
-import java.sql.Connection;
+import se.hkr.grp02.da216b.HIBDB.ECAlgorithmType;
+import se.hkr.grp02.da216b.HIBDB.ECAlgorithm;
+import se.hkr.grp02.da216b.HIBDB.ECRTLEntry;
 
-public class DBManager implements IDBManager{
+import java.sql.Connection;
+import java.util.List;
+
+public class DBManager implements IDBManager {
+
 
     /***
      * IN-CLASS CONNECTION TEST
@@ -10,5 +16,18 @@ public class DBManager implements IDBManager{
      */
     public static void main(String[] args) {
         Connection connection = IDBManager.getConnection();
+
+        System.out.println(IDBManager.insertAlgorithm(new ECAlgorithm("DUMMY [DELETE-ME]", ECAlgorithmType.comparisonSort, "nlogn", "nlogn", "n^2", false)));
+
+        List<ECAlgorithm> getAllAlgorithms = IDBManager.getAllAlgorithms();
+        List<ECAlgorithm> getAllAlgorithmsWithWorstCase = IDBManager.getAllAlgorithmsWithWorstCase("n^2");
+        List<ECRTLEntry> getAllRTLEntries = IDBManager.getAllRTLEntries();
+        List<ECRTLEntry> getAllRTLEntriesWithAlgorithmName = IDBManager.getAllRTLEntriesWithAlgorithmName("MergeSort");
+        List<ECRTLEntry> getAllRTLEntriesWithWorkLoad1 = IDBManager.getAllRTLEntriesWithWorkLoad(100);
+        List<ECRTLEntry> getAllRTLEntriesWithWorkLoad2 = IDBManager.getAllRTLEntriesWithWorkLoad(100, 1000);
+
+
+        System.out.println("end");
+     
     }
 }
