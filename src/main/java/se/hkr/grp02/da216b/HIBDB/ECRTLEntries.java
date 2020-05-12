797d1a749c92d516c53c2ce0a0fb67eb484e467f
Vincenzo <vincebshock@gmail.com>
2020-05-12 07:47:23 +0200

    ECRTLEntries getter/setter added

--- src/main/java/se/hkr/grp02/da216b/HIBDB/ECRTLEntries.java
+++ src/main/java/se/hkr/grp02/da216b/HIBDB/ECRTLEntries.java
@@ -25,5 +25,63 @@ public class ECRTLEntries {
     @Column(name = "targetOS")
     String targetOS;
 
+    @Column(name = "workload")
+    String workload;
 
+    public ECRTLEntries(int id, Timestamp timestamp, String rtrResult, String targetlang, String targetOS, String workload) {
+        this.id = id;
+        this.timestamp = timestamp;
+        this.rtrResult = rtrResult;
+        this.targetlang = targetlang;
+        this.targetOS = targetOS;
+        this.workload = workload;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getRtrResult() {
+        return rtrResult;
+    }
+
+    public void setRtrResult(String rtrResult) {
+        this.rtrResult = rtrResult;
+    }
+
+    public String getTargetlang() {
+        return targetlang;
+    }
+
+    public void setTargetlang(String targetlang) {
+        this.targetlang = targetlang;
+    }
+
+    public String getTargetOS() {
+        return targetOS;
+    }
+
+    public void setTargetOS(String targetOS) {
+        this.targetOS = targetOS;
+    }
+
+    public String getWorkload() {
+        return workload;
+    }
+
+    public void setWorkload(String workload) {
+        this.workload = workload;
+    }
 }
