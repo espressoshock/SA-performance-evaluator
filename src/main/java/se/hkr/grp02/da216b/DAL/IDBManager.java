6525455f65ee9db26b98e446ce2113afc6d1b825
Vincenzo <vincebshock@gmail.com>
2020-05-12 09:12:29 +0200

    IDBManager: getAlgorithmWithName added

--- src/main/java/se/hkr/grp02/da216b/DAL/IDBManager.java
+++ src/main/java/se/hkr/grp02/da216b/DAL/IDBManager.java
@@ -4,6 +4,7 @@ import com.mysql.cj.jdbc.Driver;
 import se.hkr.grp02.da216b.HIBDB.ECAlgorithm;
 import se.hkr.grp02.da216b.HIBDB.ECAlgorithmType;
 import se.hkr.grp02.da216b.utilities.Algorithms;
+import se.hkr.grp02.da216b.utilities.Result;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -15,14 +16,16 @@ public interface IDBManager {
     public static final String CONNECTION_USER = "sapevaluatordb";
     public static final String CONNECTION_PASSWORD = "Kb4N70U3mA??";
 
-    public static Connection getConnection(){
+    public static Connection getConnection() {
         try {
             java.sql.DriverManager.registerDriver(new Driver());
             return DriverManager.getConnection(CONNECTION_URL, CONNECTION_USER, CONNECTION_PASSWORD);
         } catch (SQLException ex) {
             throw new RuntimeException("Error connecting to the database", ex);
         }
-    };
+    }
+
+    ;
     /******************* ALGORITHM *******************/
 
     /***
@@ -30,9 +33,9 @@ public interface IDBManager {
      * @param algorithm to be added
      * @return transaction result
      */
-    public static Boolean insertAlgorithm(ECAlgorithm algorithm){
+    public static Boolean insertAlgorithm(ECAlgorithm algorithm) {
         Connection connection = IDBManager.getConnection();
-        try{
+        try {
             PreparedStatement ps = connection.prepareStatement("INSERT INTO algorithms VALUES (?, ?,?,?,?,?)");
             ps.setString(1, algorithm.getName());
             ps.setString(2, algorithm.getType().toString());
@@ -40,21 +43,24 @@ public interface IDBManager {
             ps.setString(4, algorithm.getComplexityAverageCase());
             ps.setString(5, algorithm.getComplexityWorstCase());
             ps.setInt(6, algorithm.getStabilityNumber());
-            if(ps.executeUpdate() == 1) return true;
-        }catch (SQLException e){
+            if (ps.executeUpdate() == 1) return true;
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return false;
-    };
-    public static List<ECAlgorithm> getAllAlgorithms(){
-        Connection connection = IDBManager.getConnection();
-        List<ECAlgorithm>ECAlgorithms = new ArrayList<>();
+    }
 
-        try{
+    ;
+
+    public static List<ECAlgorithm> getAllAlgorithms() {
+        Connection connection = IDBManager.getConnection();
+        List<ECAlgorithm> ECAlgorithms = new ArrayList<>();
+
+        try {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM algorithms");
 
-            while(resultSet.next()){
+            while (resultSet.next()) {
                 ECAlgorithms.add(new ECAlgorithm(
                         resultSet.getString("name"),
                         ECAlgorithmType.valueOf(resultSet.getString("type")),
@@ -66,7 +72,30 @@ public interface IDBManager {
             }
             return ECAlgorithms;
 
-        }catch (SQLException e){
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static ECAlgorithm getAlgorithmWithName(String algorithmName) {
+        Connection connection = IDBManager.getConnection();
+        try {
+            PreparedStatement ps = connection.prepareStatement("SELECT * FROM algorithms WHERE name=?");
+            ps.setString(1, algorithmName);
+
+            ResultSet resultSet = ps.executeQuery();
+
+            if (resultSet.next())
+                return new ECAlgorithm(
+                        resultSet.getString("name"),
+                        ECAlgorithmType.valueOf(resultSet.getString("type")),
+                        resultSet.getString("complexity_bestCase"),
+                        resultSet.getString("complexity_averageCase"),
+                        resultSet.getString("complexity_worstCase"),
+                        resultSet.getBoolean("stability"));
+
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
